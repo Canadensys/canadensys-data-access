cccd0b8325d373f7acad1e97b6e2a4591c68cb21
Pedro Guimarães <tigreped@gmail.com>
2014-01-27 19:16:14 -0200

    # duplicated sql query used to add columns to public schema and applied it to effect buffer schema as well

--- src/main/resources/script/occurrence/create_geospatial_columns.sql
+++ src/main/resources/script/occurrence/create_geospatial_columns.sql
@@ -1,4 +1,12 @@
+-- Public schema:
 --WSG84 latitude/longitude
 SELECT AddGeometryColumn('public','occurrence','the_geom', 4326, 'POINT', 2 );
 --WebMercator projection
 SELECT AddGeometryColumn('public','occurrence','the_geom_webmercator', 3857, 'POINT', 2 );
+
+-- Buffer schema:
+--WSG84 latitude/longitude
+SELECT AddGeometryColumn('public','occurrence','the_geom', 4326, 'POINT', 2 );
+--WebMercator projection
+SELECT AddGeometryColumn('public','occurrence','the_geom_webmercator', 3857, 'POINT', 2 );
+
