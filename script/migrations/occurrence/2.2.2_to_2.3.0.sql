431ff1f3b93542a28a13f7fc98ff4965b8c01cd4
Christian Gendreau <christiangendreau@gmail.com>
2014-01-31 14:58:47 -0500

    missing entries for 2.2.2 to 2.3.0 migration

--- script/migrations/occurrence/2.2.2_to_2.3.0.sql
+++ script/migrations/occurrence/2.2.2_to_2.3.0.sql
@@ -3,3 +3,6 @@ ALTER TABLE buffer.resource_contact RENAME dataset_shortname TO sourcefileid;
 
 ALTER TABLE resource_contact RENAME dataset_title TO resource_name;
 ALTER TABLE buffer.resource_contact RENAME dataset_title TO resource_name;
+
+ALTER TABLE occurrence ADD COLUMN associatedmediamime text;
+ALTER TABLE buffer.occurrence ADD COLUMN associatedmediamime text;
