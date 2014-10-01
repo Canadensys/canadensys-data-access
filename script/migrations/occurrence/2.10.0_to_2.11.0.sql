153cf64fc1683b40dfaf96692dc07a035e2cede7
Christian Gendreau <christiangendreau@gmail.com>
2014-10-01 11:16:09 -0400

    prepare for 2.11.0 release

--- script/migrations/occurrence/2.10.0_to_2.11.0.sql
+++ script/migrations/occurrence/2.10.0_to_2.11.0.sql
@@ -1,10 +1,28 @@
-ALTER TABLE occurrence_extension ADD COLUMN auto_id bigint NOT NULL;
-ALTER TABLE occurrence_extension ADD COLUMN dwcaid character varying(75);
-ALTER TABLE occurrence_extension ADD COLUMN sourcefileid character varying(50);
-ALTER TABLE occurrence_extension ADD COLUMN resource_uuid character varying(50);
+--This table should be empty but if not please, transfer the content before running drop table
+DROP TABLE occurrence_extension;
+CREATE TABLE IF NOT EXISTS occurrence_extension
+(
+	auto_id bigint NOT NULL,
+	dwcaid character varying(75),
+	sourcefileid character varying(50),
+	resource_uuid character varying(50),
+	ext_type character varying(25), 
+	ext_version character varying(10), 
+	ext_data hstore,
+	CONSTRAINT occurrence_extension_pkey PRIMARY KEY (auto_id)
+);
 
---This table should be empty but if not please, transfer the content of 'id' into 'auto_id' and then delete the 'id' column
-ALTER TABLE occurrence_extension DROP COLUMN IF EXISTS id;
+CREATE SEQUENCE IF NOT EXISTS buffer.occurrence_extension_id_seq;
+CREATE TABLE IF NOT EXISTS buffer.occurrence_extension
+(
+	auto_id bigint DEFAULT nextval('buffer.occurrence_extension_id_seq') NOT NULL,
+	dwcaid character varying(75),
+	sourcefileid character varying(50),
+	resource_uuid character varying(50),
+	ext_type character varying(25), 
+	ext_version character varying(10), 
+	ext_data hstore
+);
 
 ALTER TABLE resource_management RENAME key TO resource_uuid;
 
